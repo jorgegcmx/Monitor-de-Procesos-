a4faa33711d9e62312ef4b588aa8a017123faf4d
jorgegcmx <jorgegcmx@gmail.com>
2019-07-25 17:55:40 -0500

    info_php

---
+++ phpinfo.php
@@ -0,0 +1,16 @@
+<?php
+//phpinfo();
+/*$serverName = "192.1.1.218"; //serverName\instanceName
+
+$connectionInfo = array( "Database"=>"AGQSLAPP", "UID"=>"master", "PWD"=>"");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Conexión establecida.<br />";
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}*/
+
+
+?>
