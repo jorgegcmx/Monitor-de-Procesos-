443814d466400f7282a13f0e5fb40c46a3cb74a7
Jorge Antonio <jorgegcmx@gmail.com>
2020-03-30 14:04:02 -0600

    Graficas mortalidad

--- pedidos.php
+++ pedidos.php
@@ -23,13 +23,10 @@ $kilos = array_column($dato, 'Peso');
                 <div class="form-group">
                  <label for="exampleInputPassword1">Año</label>                
                  <select  class="form-control" name="ano" >
-                 <option value="2017">2017</option>
-                 <option value="2018">2018</option>
-                 <option value="2019">2019</option>
                  <option value="2020">2020</option>
-                 <option value="2021">2021</option>
-                 <option value="2022">2022</option>
-                 <option value="2023">2023</option>
+                 <option value="2019">2019</option>
+                 <option value="2018">2018</option>
+                 <option value="2017">2017</option>                 
                  </select>
                 </div>
                 <div class="form-group">
